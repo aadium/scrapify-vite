451235f1dbf30ee9674a5b193f1fbed65c4684e9
Aadi Umrani <aadi.p.um@gmail.com>
2024-07-01 22:49:51 -0400

    Added a loader to the create scraper page

--- src/pages/addScraper.jsx
+++ src/pages/addScraper.jsx
@@ -7,6 +7,7 @@ export default function AddScraper() {
     const [bearerToken, setBearerToken] = useState('');
     const [url, setUrl] = useState('');
     const [selectors, setSelectors] = useState({});
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
@@ -16,6 +17,7 @@ export default function AddScraper() {
         const token = bearerToken;
 
         try {
+            setLoading(true);
             const response = await fetch(endpoint, {
                 method: 'POST',
                 headers: {
@@ -28,6 +30,7 @@ export default function AddScraper() {
                 throw new Error('Failed to create scraper: ' + response.statusText);
             }
             const data = await response.json();
+            setLoading(false);
             alert('Scraper created successfully');
             navigate('/dashboard');
         } catch (error) {
@@ -123,7 +126,22 @@ export default function AddScraper() {
                     ))}
                     <button type="button" onClick={addSelector} className="group transition relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-black bg-white hover:bg-zinc-300">Add Selector</button>
                 </div>
-                <button type="submit" className="w-full py-2 px-4 bg-orange-600 hover:bg-orange-700 text-white rounded-md transition ">Submit</button>
+                <button type="submit" className="w-full py-2 px-4 bg-orange-600 hover:bg-orange-700 text-white rounded-md transition ">
+                    {
+                        loading ? (
+                            <div className="flex justify-center items-center h-max">
+                                <div className="inline-block h-5 w-5 animate-spin rounded-full border-4 border-solid border-current border-e-transparent text-warning motion-reduce:animate-[spin_2s_linear_infinite]"
+                                    role="status">
+                                    <span className="absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 clip:rect(0,0,0,0)">Loading...</span>
+                                </div>
+                            </div>
+                        ) : (
+                            <div>
+                                Create Scraper
+                            </div>
+                        )
+                    }
+                </button>
             </form>
         </div>
     );
