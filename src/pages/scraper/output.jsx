ad1139d7efc52680663a34ff560fc50cb9dcee46
Aadi Umrani <aadi.p.um@gmail.com>
2024-07-03 14:04:31 -0400

    Created a separate utility function to check authentication state and set the bearer token

--- src/pages/scraper/output.jsx
+++ src/pages/scraper/output.jsx
@@ -6,6 +6,7 @@ import sparkles from '../../assets/sparkler.png';
 import { requestGroqAi } from '../../utils/groq';
 import Modal from '../../utils/modal';
 import Header from '../../widgets/header';
+import useAuth from '../../utils/useAuth';
 
 export default function Output() {
     const { id } = useParams();
@@ -99,39 +100,7 @@ export default function Output() {
         downloadLink.click();
     }
 
-    useEffect(() => {
-        const tokenString = localStorage.getItem('token');
-        let token = null;
-        try {
-            token = JSON.parse(tokenString);
-        } catch (e) {
-            console.error('Error parsing token:', e);
-        }
-        if (!token) {
-            navigate('/signin');
-        }
-        fetch(`${import.meta.env.VITE_API_URL}/auth/verifyToken`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Token validation failed: ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data.message);
-                setBearerToken(token);
-            })
-            .catch(error => {
-                console.error('Error validating token:', error);
-                navigate('/signin');
-            });
-    }, []);
+    useAuth(setBearerToken);
 
     useEffect(() => {
         if (bearerToken !== '') {
